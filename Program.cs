2160b5acf2479861541fd2f0b53d289698bfa1dc
OGÜN ERKUTAY <77350593+ogunerkutay@users.noreply.github.com>
2023-02-01 20:30:17 +0300

    Example console application for arrays in c#

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUSIC_PLAYLIST
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] listeler = new string[1] { "Liste Boş" };
+            string girilendeger = string.Empty;
+            string listeismi = string.Empty;
+            do
+            {
+                Console.WriteLine("Şarkıları listelemek için 'listele'\nŞarkı eklemek için 'sarki ekle'\nŞarkı çıkartmak için 'sarki cikart'\nÇıkmak için 'tamam' yazınız!");
+                Console.WriteLine(new String('-', 100));
+                girilendeger = Console.ReadLine().Trim().ToLower();
+
+                switch (girilendeger)
+                {
+                    case "tamam":
+                        break;
+                    case "listele":
+                        Listele(listeler);
+                        break;
+                    case "sarki ekle":
+                        listeler = SarkiEkle(listeler);
+                        break;
+                    case "sarki cikart":
+                        listeler = SarkiCikart(listeler);
+                        break;
+
+                    default:
+                        Console.WriteLine(new String('-', 100));
+                        Console.WriteLine("Böyle bir komut bulunamadı!");
+                        break;
+                }
+            } while (girilendeger != "tamam");
+            Console.WriteLine("Uygulamamızı puanlamayı unutmayın, bizi tercih ettiğiniz için teşekkür ederiz!");
+        }
+        static void Listele(string[] args)
+        {
+            Console.WriteLine(new String('-', 100));
+            Console.WriteLine("Listenizin son hali : ");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+            Console.WriteLine(new String('-', 100));
+        }
+        static string[] SarkiEkle(string[] args)
+        {
+            Console.WriteLine(new String('-', 100));
+            Console.WriteLine("Şarkınız için bir isim belirtiniz!");
+            string listeismi;
+            do
+            {
+                listeismi = Console.ReadLine().Trim();
+                if (listeismi == string.Empty)
+                {
+                    Console.WriteLine("Şarkı isimleri boşluk olamaz!");
+                }
+            } while (listeismi == string.Empty);
+            
+            if (args[0] == "Liste Boş")
+            {
+                args[0] = listeismi;
+
+                Listele(args);
+                return args;
+            }
+            else
+            {
+                string[] yeniListe = new string[args.Length + 1];
+                Array.Copy(args, yeniListe, args.Length);
+                yeniListe[yeniListe.Length - 1] = listeismi;
+                Listele(yeniListe);
+                return yeniListe;
+            }
+
+        }
+
+
+        static string[] SarkiCikart(string[] args)
+        {
+            Console.WriteLine(new String('-', 100));
+            Console.WriteLine("Çıkartmak istediğiniz şarkı için bir isim belirtiniz!");
+            Listele(args);
+            string listeismi;
+            do
+            {
+                listeismi = Console.ReadLine().Trim();
+                if (!args.Contains(listeismi))
+                {
+                    Console.WriteLine("Çıkartmak istediğiniz şarkı ismi yoktur!\nLütfen aşağıdaki listeden şarkı çıkartmak için bir şarkı isim seçiniz : ");
+                    Listele(args);
+                }
+
+            } while (listeismi == string.Empty || !args.Contains(listeismi));
+            string cevap;
+            do
+            {
+                Console.WriteLine(new String('-', 100));
+                Console.WriteLine(listeismi + " listenizden silinecektir emin misiniz? evet - hayır");
+                Console.WriteLine(new String('-', 100));
+                cevap = Console.ReadLine().Trim();   
+            } while (cevap!="evet" && cevap!="hayır");
+
+            if (cevap == "evet")
+            {
+                if (args.Length == 1)
+                {
+                    args[Array.IndexOf(args, listeismi)] = "Liste Boş";
+                }
+                else
+                {
+                    args[Array.IndexOf(args, listeismi)] = String.Empty;
+                    args = args.Where(listeisimleri => !string.IsNullOrEmpty(listeisimleri)).ToArray();
+                }
+
+
+                Listele(args);
+
+            }
+
+            return args;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MUSIC PLAYLIST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MUSIC PLAYLIST")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ab2eb27f-760e-4fc9-9c04-37df7e650962")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
